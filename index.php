21f3dacf6fcbfa776c8d2656889e08d9dea28a09
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-04-26 17:02:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
 
 <h1>My first PHP page</h1>
 
-<?php
-echo "Hello World!";
-
-echo "Hello GCH0701";
+<?php 
+$sql = "SELECT id, name FROM label";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
 ?>
 
 </body>
