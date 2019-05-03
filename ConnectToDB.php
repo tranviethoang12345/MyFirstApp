85671ef2ffa212aca273045baedf44d00465f466
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-05-03 14:39:20 +0700

    DB

---
+++ ConnectToDB.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+ini_set('display_errors', 1);
+echo "Hello abc XYZ!";
+?>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "FPT Greenwich";
+$x = 5;
+$y = 5;
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+if (empty(getenv("DATABASE_URL"))){
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+$sql = "SELECT id, name FROM label";
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+?>
+</body>
+</html>
---
+++ db.php
@@ -0,0 +1,20 @@
+<?php 
+$sql = "SELECT id, name FROM label";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+?>
