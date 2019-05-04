5cce94b4b0b590eed7d5607ebc5e7b425bc556d3
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-05-04 10:41:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,31 @@
 
 <h1>My Max Page <3</h1>
 
-<?php require_once("db.php") ?>
+<?php
+	$sql = "SELECT id, name FROM label"; 
+
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	$stmt = $pdo->prepare($sql);
+
+	//Thiết lập kiểu dữ liệu trả về
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+
+	echo "<ul>";
+	foreach ($resultSet as $row) {
+		echo "<li>" . $row['name'] . "</li>";
+	}
+		echo "</ul>";
+	?>
 
 </body>
 </html>x
