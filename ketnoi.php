2efc101bd7398cd9a1198a47fb28b4e9a8a10cbc
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-05-06 12:36:02 +0700

    Create ketnoi.php

---
+++ ketnoi.php
@@ -0,0 +1,7 @@
+<?php
+$connection = pg_connect("host=ec2-75-101-133-29.compute-1.amazonaws.com port=5432 dbname=
+d655s56p6gj9ra user=xkjbzfeeqdhctq password=f595010b1c372f3c41aff5eafd322121352cbfc30472927177c8927749e3af68");  
+if(!$connection) {
+ 	die("Database connection failed");
+}
+?>
