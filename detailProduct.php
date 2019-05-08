b762474e352ae7e9a29069d9377f3b4b3fc7ee3a
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-05-08 15:09:21 +0700

    Update detailProduct.php

--- detailProduct.php
+++ detailProduct.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Detail</title>
+	<title>DetailHeroku</title>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -17,36 +17,24 @@
 	<main>
 	<!-- Main Layout -->
 	<div class="container">
-		<div class="row">
-			<!--Sidebar-->
-	 		<?php require_once("sidebar.php") ?>
-	        <!--/.Sidebar-->
-		</div>
+		<h2 class="h2-responsive">Details<h2>
+      	<?php 
+				include 'dbconnector.php';
+				$proid =$_GET['proid'];
+            $sql = "SELECT * FROM product WHERE proid = '$proid'";
+            $result = pg_query($connection,$sql);
+            if (pg_num_rows($result) > 0) {
+            // output data of each row
+            while($key = pg_fetch_assoc($result)) {
+            	$proid = $key['proid'];
+              	$proimage = $key['proimage'];
+              	$proname = $key['proname'];
+              	$prodes = $key['prodes'];
+              	$proprice = $key['proprice'];
+              	$prodescribe = $key['prodescribe'];
+       	?>
+	          
 
-			<!--Main column-->
-	        <div class="col-lg-8">
-					<!--First row-->
-					<h2 class="h2-responsive">Details<h2>
-		          	<?php 
-
-
- 						include 'dbconnector.php';
- 						$proid =$_GET['proid'];
-			            $sql = "SELECT * FROM product WHERE proid = '$proid'";
-			            $result = pg_query($connection,$sql);
-			            if (pg_num_rows($result) > 0) {
-			            // output data of each row
-			            while($key = pg_fetch_assoc($result)) {
-			            	$proid = $key['proid'];
-			              	$proimage = $key['proimage'];
-			              	$proname = $key['proname'];
-			              	$prodes = $key['prodes'];
-			              	$proprice = $key['proprice'];
-			              	$prodescribe = $key['prodescribe'];
-		           	?>
-			          
-			        <div class="row">
-			            <div class="col-lg-12">
 			            <!--Card-->
 			            <div class="card">
 			                <!--Card image-->
@@ -75,10 +63,6 @@
             		</div>
             		<?php }} ?>
           		</div>
-          		<!--/row-->
-			</div>
-			<!--/.Main column-->
-		</div>
 	</main>
 
 	<footer class="bg-secondary text-white text-center" style="height: 100px;">
