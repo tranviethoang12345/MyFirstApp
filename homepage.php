2b8dc255dc751172e450e7ee292d756c7a6e209c
Viet Hoang Max <tranviethoang12345@gmail.com>
2019-05-08 13:24:44 +0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -41,14 +41,15 @@
 			            $result = pg_query($connection,$sql);
 			            if (pg_num_rows($result) > 0) {
 			            // output data of each row
-			            while($key = pg_fetch_assoc($result)) {
-			            	$proid = $key['proid'];
-			              	$proimage = $key['proimage'];
-			              	$proname = $key['proname'];
-			              	$prodes = $key['prodes'];
-			              	$proprice = $key['proprice'];
+			            while($rows = pg_fetch_assoc($result)) {
+			            	$proid = $rows['proid'];
+			              	$proimage = $rows['proimage'];
+			              	$proname = $rows['proname'];
+			              	$prodes = $rows['prodes'];
+			              	$proprice = $rows['proprice'];
 		          	?>
 			         <div class="row">
+			         	<?php foreach ($rows as $key) { ?>
 		        		<div class="col-lg-4">
 		              		<div class="card" style="width: 15rem; height: 34em; margin-bottom: 0.5em">
 				                <div style="height: 18em">
@@ -66,6 +67,7 @@
 			               	 	</div>
 			              	</div>
 		        		</div>
+		        		<?php } ?>
 			        </div>
 
 			        <?php }} ?>
